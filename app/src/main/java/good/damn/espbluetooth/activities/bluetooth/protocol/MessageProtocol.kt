39627ce2b322834cf19c88d70524d70076fa7a68
GoodDamn <grigorydum@gmail.com>
2024-06-11 17:36:57 +0300

    Fix bug with blocking thread

--- app/src/main/java/good/damn/espbluetooth/activities/bluetooth/protocol/MessageProtocol.kt
+++ app/src/main/java/good/damn/espbluetooth/activities/bluetooth/protocol/MessageProtocol.kt
@@ -1,11 +1,16 @@
 package good.damn.espbluetooth.activities.bluetooth.protocol
 
+import android.util.Log
 import good.damn.espbluetooth.Application
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 
 class MessageProtocol {
+
+    companion object {
+        private const val TAG = "MessageProtocol"
+    }
 
     fun getMessage(
         inp: InputStream
@@ -14,6 +19,7 @@ class MessageProtocol {
             100
         )
 
+        var attempts = 0
         var n = 0
         while (n != 0x0A) { // Line feed
 
@@ -21,6 +27,14 @@ class MessageProtocol {
                 break
             }
 
+            if (inp.available() == 0) {
+                if (attempts >= 100000) {
+                    break
+                }
+                attempts++
+                continue
+            }
+            attempts = 0
             n = inp.read()
             if (n == 0) {
                 continue
